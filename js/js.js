e4330fbc60421d47a4f72a25cecafd0e5c60b799
Markus <markus@iMac-Markus.local>
2017-09-14 22:26:14 +0300

    Пост финал 2

---
+++ js/js.js
@@ -0,0 +1,7 @@
+ var link = document.querySelector(".trip-screen button");
+ var popup = document.querySelector(".popup-form");
+
+ link.addEventListener("click", function (evt) {
+ 	evt.preventDefault();
+ 	popup.classList.toggle("popup-form-hidden");
+ });
